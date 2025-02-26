18650acd22115a99d425e012f6b4f1472da70f3c
Juan D. Collada Vasquez <cvjuan270@gmail.com>
2025-02-26 09:33:40 -0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Estados de flujo de ventas (shokale)",
+    "version": "17.0.1.0.1",
+    "summary": """
+        Agrega nuevos estados en `sale` de `sale.order`
+     """,
+    "category": "sale",
+    "author": "Juan D. Collado Vasquez",
+    "website": "https://tagre.pe",
+    "depends": [
+        "sale_management",
+    ],
+    "data": [
+        'views/sale_order_views.xml'
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ models/sale_order.py
@@ -0,0 +1,28 @@
+from odoo import models, fields
+
+SALE_ORDER_PRODUCTION_STATE = [
+    ('without', 'Sin Producción'),
+    ('production', 'En Producción'),
+    ('production_done', 'Producción Terminada')
+]
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    production_state = fields.Selection(
+        selection=SALE_ORDER_PRODUCTION_STATE,
+        string="Producción",
+        readonly=True, copy=False, index=True,
+        tracking=3,
+        default='without')
+
+    def action_production(self):
+        self.ensure_one()
+        self.production_state = 'production'
+        self.message_post(body="La orden de venta ha pasado a producción.")
+
+    def action_production_done(self):
+        self.ensure_one()
+        self.production_state = 'production_done'
+        self.message_post(body="Producción terminada. Ahora se puede facturar.")
